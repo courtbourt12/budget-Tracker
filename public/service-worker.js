1a4051cab6a7b679a28d640a91adbf1e02679bb6
courtney long <courtneylong877@gmail.com>
2021-11-22 09:58:24 -0500

    Caches are being stored

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,76 @@
-var CACHE_NAME = 'my-site-cache-v1';
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 var FILES_TO_CACHE = [
     '/',
-    '/public/icons/icon-192x192.png',
-    '/public/icons/icon-512x512.png',
-    '/public/styles.css',
-    'public/index.js',
-    'public/manifest.json'
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    '/styles.css',
+    '/index.js',
+    '/manifest.json',
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ]
+
+// Installing the things from the cache.  Helpful for when it comes back online.
+
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+
+// Checks to remove or keep any old content, deletes anything we don't need, and then creates a new service worker.
+
+  
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// For storing api data from the /api webpages.
+  
+self.addEventListener("fetch", function(evt) {
+    // cache successful requests to the API
+    if (evt.request.url.includes("/api")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then((res) => {
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+              return res;
+            })
+            .catch((err) => {
+              return cache.match(evt.request);
+            });
+        }).catch((err) => console.log(err))
+      );
+  
+      return;
+    }
+  
+    // If the offline fetch was successful, then store the data, if not it will ignore it.
+
+    evt.respondWith(
+      caches.match(evt.request).then(function(response) {
+        return response || fetch(evt.request);
+      })
+    );
+  });
