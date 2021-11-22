4ef11c48970f9b0ce6d8e094305be93fa7b8ce84
courtney long <courtneylong877@gmail.com>
2021-11-21 20:48:01 -0500

    added manifest.json and files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,9 @@
-console.log("this is running")
+var CACHE_NAME = 'my-site-cache-v1';
+var FILES_TO_CACHE = [
+    '/',
+    '/public/icons/icon-192x192.png',
+    '/public/icons/icon-512x512.png',
+    '/public/styles.css',
+    'public/index.js',
+    'public/manifest.json'
+]
